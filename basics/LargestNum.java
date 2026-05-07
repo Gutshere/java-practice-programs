8b214b4f381b5e7b636efd76a9e4f03b217b90a3
rajesh <147789852+Gutshere@users.noreply.github.com>
2026-05-07 20:59:53 +0530

    Refactor LargestNum.java for readability

--- basics/LargestNum.java
+++ basics/LargestNum.java
@@ -3,18 +3,25 @@ import java.util.*;
 public class LargestNum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc = new Scanner(System.in);
-while(true) {try { System.out.println("Enter three numbers : ");
-int a = sc.nextInt();
-int b = sc.nextInt();
-int c = sc.nextInt();
-if(a>b&&a>c) {
-	System.out.println("The largest number among them is : "+a);
-}
-else if(b>c){System.out.println("The largest number among them is : "+b);}
-else if(a==b&&b==c) {System.out.println("All numbers are equal.");}
-else{System.out.println("The largest number among them is : "+c);}}
-catch(InputMismatchException e) { System.out.println("Invalid input!");}
-sc.next();}}
+		
+           Scanner sc = new Scanner(System.in);
+		
+           while(true) {
+			   try { System.out.println("Enter three numbers : ");
+                     int a = sc.nextInt();
+                     int b = sc.nextInt();
+                     int c = sc.nextInt();
+					
+                     if(a>b&&a>c) {
+	                     System.out.println("The largest number among them is : "+a);
+                     }
+                     else if(b>c){System.out.println("The largest number among them is : "+b);}
+                     else if(a==b&&b==c) {System.out.println("All numbers are equal.");}
+                     else{System.out.println("The largest number among them is : "+c);}
+			  }
+              catch(InputMismatchException e) { System.out.println("Invalid input!");}
+			   
+             sc.next();
+		   }
+	}
 }
