60461328ea11ea75e835abe63dc022c19ae19b1e
rajesh <147789852+Gutshere@users.noreply.github.com>
2026-05-09 10:36:42 +0530

    Refactor Calculator class and improve user prompts

--- math programs/Calculator.java
+++ math programs/Calculator.java
@@ -1,31 +1,33 @@
-package ippo;
 import java.util.*;
-public class Calulu {
+public class Calculator {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc = new Scanner(System.in);
-while(true) {try {System.out.println("Enter two numbers : ");
-int a = sc.nextInt();
-int b = sc.nextInt();
-int c;
-while(true) {
-System.out.println("Choose your option : \n1. Add \n2. Diff \n3. Mul \n4. Div \n5. Rem");
-c = sc.nextInt();
-if(c>=1&&c<=5) {break;}
-else {System.out.println("Invalid input!");}}
-switch(c) {
-case 1: System.out.println("Result : "+(a+b));
-break;
-case 2: System.out.println("Result : "+(a-b));
-break;
-case 3: System.out.println("Result : "+(a*b));
-break;
-case 4: System.out.println("Result : "+(a/b));
-break;
-case 5: System.out.println("Result : "+(a%b));
-break;
-}}
-catch(InputMismatchException w) {System.out.println("invalid input!");
-sc.next();}
+		
+		Scanner sc = new Scanner(System.in);
+		while(true) {try {System.out.println("Enter two numbers : ");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int choice;
+		while(true) {
+		System.out.println("Choose your option : \n1. Addition \n2. Difference \n3. Multiplication \n4. Division \n5. Reminder");
+		choice = sc.nextInt();
+		if(choice >= 1 && choice <= 5) {break;}
+		else {System.out.println("Invalid input!");}
+		}
+		switch(choice) {
+		case 1: System.out.println("Result : "+(num1+num2));
+		break;
+		case 2: System.out.println("Result : "+(num1-num2));
+		break;
+		case 3: System.out.println("Result : "+(num1*num2));
+		break;
+		case 4: System.out.println("Result : "+(num1/num2));
+		break;
+		case 5: System.out.println("Result : "+(num1%num2));
+		break;
+		}
+						 }
+		catch(InputMismatchException w) {System.out.println("invalid input!");
+		sc.next();}
+					}
+	}
 }
-}}
