242ab2593985cb307d468f27f907fa005a289564
rajesh <147789852+Gutshere@users.noreply.github.com>
2026-05-09 10:30:01 +0530

    Refactor VowelCount for clarity and variable names

--- Strings/VowelCount.java
+++ Strings/VowelCount.java
@@ -1,18 +1,19 @@
-package ippo;
 import java.util.Scanner;
 class VowelCount {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter any string/word : ");
-	String a = sc.nextLine();
-	String b = a.toLowerCase();
-	int c = 0;
-	char[] d = b.toCharArray();
-	for(char e : d) {
-		if(e=='a' || e=='e' || e=='i' || e=='o' || e=='u') {
-		c++;
-		}
-		else {}}
-		System.out.println("Number of vowels in the given string : "+c);
-		sc.close();}}
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any string/word : ");
+			String word1 = sc.nextLine();
+			String word2 = word1.toLowerCase();
+			int count = 0;
+			char[] a = word2.toCharArray();
+			for(char b : a) {
+				if(b=='a' || b=='e' || b=='i' || b=='o' || b=='u') {
+				c++;
+				}
+				else {}}
+			System.out.println("Number of vowels in the given string : "+count);
+			sc.close();
+	}
+}
