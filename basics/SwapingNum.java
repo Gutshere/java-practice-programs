e63e2ab1fff3cc2eae54b22601b07aff3eaea83b
rajesh <147789852+Gutshere@users.noreply.github.com>
2026-05-09 10:23:35 +0530

    Refactor SwapingNum class and variable names

--- basics/SwapingNum.java
+++ basics/SwapingNum.java
@@ -1,35 +1,33 @@
-package ippo;
 import java.util.*;
 import java.util.InputMismatchException;
-public class Swap {
+public class SwapingNum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc = new Scanner(System.in);
-while(true) { try {
-System.out.print("Enter your first number : ");
-int a = sc.nextInt();
-System.out.print("Now enter your second number : ");
-int b = sc.nextInt();
-System.out.println("(Swapping the numbers...)");
-int temp = a;
-a = b;
-b = temp;
-System.out.println("After swapping\na is : "+a);
-System.out.println("b is : "+b);
-System.out.println("Do you want to continue ? (yes/no)");
-String c = sc.next();
-if(c.equalsIgnoreCase("yes")) {
-	System.out.println("Okay");
-	continue;
-}
-else { System.out.println("Thank you");
-break;}
-}
-catch(InputMismatchException e) {
-	System.out.println("Invalid input!");
-sc.next();}}
-sc.close();
+		
+		Scanner sc = new Scanner(System.in);
+		while(true) { try {
+		System.out.print("Enter your first number : ");
+		int num1 = sc.nextInt();
+		System.out.print("Now enter your second number : ");
+		int num2 = sc.nextInt();
+		System.out.println("(Swapping the numbers...)");
+		int temp = num1;
+		num1 = num2;
+		num2 = temp;
+		System.out.println("After swapping\na is : "+num1);
+		System.out.println("b is : "+num2);
+		System.out.println("Do you want to continue ? (yes/no)");
+		String answer = sc.next();
+		if(answer.equalsIgnoreCase("yes")) {
+			System.out.println("Okay");
+			continue;
+		}
+		else { System.out.println("Thank you");
+		break;}
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Invalid input!");
+		sc.next();}}
+		sc.close();
 	}
-
 }
