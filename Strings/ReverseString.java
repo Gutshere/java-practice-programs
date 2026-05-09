4a229c79bd56af5a3c2ddf6ee9465b4321c4354d
rajesh <147789852+Gutshere@users.noreply.github.com>
2026-05-09 10:26:07 +0530

    Rename class and update reverse string implementation

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -1,16 +1,17 @@
-package ippo;
 import java.util.*;
-public class Rgear {
+public class ReverseString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter a string : ");
-String word = sc.nextLine();
-StringBuilder sb = new StringBuilder(word);
-System.out.print("Now your string is in reverse gear : ");
-/*for(int i=word.length()-1;i>=0;i--) {
-	System.out.print(word.charAt(i));
-}*/
-System.out.println(sb.reverse()); //stringbuilder method
-}}
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string : ");
+		String word = sc.nextLine();
+		StringBuilder sb = new StringBuilder(word);
+		System.out.print("Now your string is in reverse gear : ");
+		//or using for loop method
+		/*for(int i=word.length()-1;i>=0;i--) {
+			System.out.print(word.charAt(i));
+		}*/
+		System.out.println(sb.reverse()); //stringbuilder method
+	}
+}
