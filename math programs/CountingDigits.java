eccec5cb98a21ddec79764ab6ad6fb1441717099
rajesh <147789852+Gutshere@users.noreply.github.com>
2026-05-09 10:39:43 +0530

    Rename class to CountingDigits and refactor code

--- math programs/CountingDigits.java
+++ math programs/CountingDigits.java
@@ -1,16 +1,17 @@
-package nDoms;
 import java.util.*;
-public class CountDs {
+public class CountingDigits {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc= new Scanner(System.in);
-System.out.println("Enter your number : ");
-/*
- * String a = sc.next();
- * System.out.println("Number of digits in the number are : "+a.length());
- */
-int a = sc.nextInt();
-int length = String.valueOf(a).length();
-System.out.println("No.of digits : "+length);
-	}}
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your number : ");
+		//or 
+		/*
+		 * String a = sc.next();
+		 * System.out.println("Number of digits in the number are : "+a.length());
+		 */
+		int num = sc.nextInt();
+		int length = String.valueOf(num).length();
+		System.out.println("No.of digits : "+length);
+	}
+}
